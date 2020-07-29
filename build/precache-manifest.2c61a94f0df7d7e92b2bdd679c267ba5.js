52acca3bfdbf5725e2d3cbf52d921ad4bb40ef03
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-07-28 22:45:57 -0700

    window resizing

---
+++ build/precache-manifest.2c61a94f0df7d7e92b2bdd679c267ba5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9acbb1f334326be13670b5ff56cfd584",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77750c9c5aafd0b2bd19",
+    "url": "/static/css/main.cfc45a8b.chunk.css"
+  },
+  {
+    "revision": "41e5b6f4b1e2e99db2bd",
+    "url": "/static/js/2.23436deb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.23436deb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77750c9c5aafd0b2bd19",
+    "url": "/static/js/main.6aaab9c1.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "63eb279cdaba6f6366a6221aae6bbf15",
+    "url": "/static/media/blue-circles.63eb279c.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71e0103889c608b78b32bb9749b798cb.js"
+  "/precache-manifest.2c61a94f0df7d7e92b2bdd679c267ba5.js"
 );
 
 self.addEventListener('message', (event) => {
