84b220effdc12b50ba911a6952e7f2f46c1f2690
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-08-06 17:41:13 -0700

    unibuild

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const path = require('path');
 const app = require('./todoApi/todoApi');
 
 app.use(express.static(path.join(__dirname, 'build')));
-
+app.get('/uni/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'unibuild', 'index.html'));
+});
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
---
+++ unibuild/precache-manifest.92bd1e2de62074d247727e25c2a65f10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dcdc3ebf962c7dbcee2e372386040fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93e6c6cc4c0c1a852dd3",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bb90664b61138a4a8288",
+    "url": "/static/css/main.5109a02d.chunk.css"
+  },
+  {
+    "revision": "93e6c6cc4c0c1a852dd3",
+    "url": "/static/js/2.870ac2f2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.870ac2f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb90664b61138a4a8288",
+    "url": "/static/js/main.91d9d366.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "c017761f636088db71ad560d691dcfad",
+    "url": "/static/media/bg-gr-go-str.c017761f.png"
+  }
+]);
