f6b42c5f72ccf6f67ddecc33574c8a082746787d
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-08-08 17:48:39 -0700

    lag time

---
+++ front-end/precache-manifest.7fd2c3261ebeaa6719f915f1eaa6cb94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b63ad9a30808d80d9a945ded1ae4162",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b047bc0b96ef61c0b301",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "265ee21550bc6c4962f3",
+    "url": "/static/css/main.d819bcbc.chunk.css"
+  },
+  {
+    "revision": "b047bc0b96ef61c0b301",
+    "url": "/static/js/2.ed00d0a0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ed00d0a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265ee21550bc6c4962f3",
+    "url": "/static/js/main.c33267a8.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "3f36e803041089bc184536f508b96773",
+    "url": "/static/media/bg-pblue-str.3f36e803.png"
+  }
+]);
