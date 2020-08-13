0e0a0f4accad7dabb73bd5808f36a1e252122a0f
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-08-12 22:02:29 -0700

    effects, unregistered serviceworker

--- front-end/precache-manifest.0d339540cbad0042e44004ad8fced244.js
+++ front-end/precache-manifest.0d339540cbad0042e44004ad8fced244.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "55fd7e48113721744880a3981bd05208",
+    "revision": "89e817b808460d74919e2c04203a59ca",
     "url": "/index.html"
   },
   {
-    "revision": "6a85da5744137ba3a482",
+    "revision": "7187ab8e6aee7843a193",
     "url": "/static/css/2.7cf7ff0c.chunk.css"
   },
   {
-    "revision": "fdf5c2cd1b716921076e",
+    "revision": "6d537edb62c47576645c",
     "url": "/static/css/main.6dce30f4.chunk.css"
   },
   {
-    "revision": "6a85da5744137ba3a482",
-    "url": "/static/js/2.44a3c1b3.chunk.js"
+    "revision": "7187ab8e6aee7843a193",
+    "url": "/static/js/2.c050113d.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/static/js/2.44a3c1b3.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.c050113d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fdf5c2cd1b716921076e",
-    "url": "/static/js/main.90d657ee.chunk.js"
+    "revision": "6d537edb62c47576645c",
+    "url": "/static/js/main.aef3650f.chunk.js"
   },
   {
     "revision": "73635f645c271e35ff15",
--- front-end/service-worker.js
+++ front-end/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c6f0b203fafd0c181826f992b462866.js"
+  "/precache-manifest.0d339540cbad0042e44004ad8fced244.js"
 );
 
 self.addEventListener('message', (event) => {
