3b13e5c0a0f2c4d744a172a13b260e382cf81c90
Aidan Barbieux site-server <abarbieu@calpoly.edu>
2020-08-14 02:22:37 +0000

    log

--- api/middleware/logger.js
+++ api/middleware/logger.js
@@ -5,12 +5,12 @@ const logFile = './logs.txt';
 const ipRegex = /[\d\.]+$/;
 
 const logger = (req, res, next) => {
-  if (req.originalUrl === '/') {
+  if (req.originalUrl === '/' || req.originalUrl === '/index.html') {
 
     const clientIP = req.headers['x-real-ip'].match(ipRegex)[0];
     const geo = geoip.lookup(clientIP);
     // console.log(clientIP);
-    const log = `${moment().tz("America/Los_Angeles")} : ${geo.country} ${geo.region} ${geo.city}`;
+    const log = `${moment().tz("America/Los_Angeles")} : ${geo.country} ${geo.region} ${geo.city} : ${req.headers['x-real-ip']}`;
 
     fs.appendFile(logFile, log + '\n', 'utf8', (err) => {
       if (err) {
