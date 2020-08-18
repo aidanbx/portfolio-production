3b331672cf4f0c01ab05600a6ede9931a13da82c
Aidan Barbieux <abarbieu@calpoly.edu>
2020-08-18 01:18:22 -0700

    obj logger

--- api/middleware/logger.js
+++ api/middleware/logger.js
@@ -1,53 +1,77 @@
 const moment = require('moment-timezone');
 const geoip = require('geoip-lite');
 const fs = require('fs');
-const logFile = './logs.txt';
 const ipRegex = /[\d\.]+$/;
 
-const getLog = (req) => {
-  let clientIP;
-  try {
-    clientIP = req.headers['x-real-ip'].match(ipRegex)[0];
-  } catch (err) {
-    clientIP = (req.headers['x-forwarded-for'] || req.connection.remoteAddress)
-      .match(ipRegex)[0];
-  }
-  if (clientIP) {
-    try {
-      console.log(clientIP);
-      const geo = geoip.lookup(clientIP);
-      // console.log(clientIP);
-      const log = `${moment().tz(
-        'America/Los_Angeles'
-      )} : ${geo.country} ${geo.region} ${geo.city} : ${req.headers[
-        'x-real-ip'
-      ]}`;
-      return log;
-    } catch (err) {
-      return clientIP;
-    }
-  }
-  return "Couldn't get IP addr";
+const getLog = (ip) => {
+	return logs[ip];
 };
 
-const logger = (req, res, next) => {
-  if (req.originalUrl === '/' || req.originalUrl === '/index.html') {
-    try {
-      const log = getLog(req);
-
-      fs.appendFile(logFile, log + '\n', 'utf8', (err) => {
-        if (err) {
-          throw err;
-        } else {
-          console.log(log);
-        }
-      });
-    } catch (error) {
-      console.error(error);
-      next();
-    }
-  }
-  next();
+const makeLog = (req) => {
+	let clientIP;
+	let log = {};
+	let tmpIP;
+	try {
+		log.time = `${moment().tz('America/Los_Angeles')}`;
+		log.method = req.method;
+		log.path = req.path;
+		log.domain = req.hostname;
+		log.protocol = req.protocol;
+		log.url = req.originalUrl;
+		log.subdomains = req.subdomains;
+	} catch (err) {
+		log[err] = err;
+	}
+	try {
+		if (req.headers['x-real-ip']) {
+			tmpIP = req.headers['x-real-ip'];
+		} else if (req.protocol === 'http') {
+			tmpIP = req.socket.remoteAddress;
+		} else if (req.protocol === 'https') {
+			tmpIP = req.connection.socket.remoteAddress;
+		} else {
+			tmpIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+		}
+		clientIP = tmpIP.match(ipRegex)[0];
+	} catch (err) {
+		log[err] = err.message;
+		clientIP = undefined;
+	}
+	if (clientIP) {
+		log.ip = clientIP;
+		try {
+			log.geo = geoip.lookup(clientIP);
+		} catch (err) {
+			log[err.name] = err;
+		}
+	} else {
+		log.ip = undefined;
+	}
+	return log;
 };
 
-module.exports = { getLog, logger };
+const logger = (req, res, next, logs) => {
+	const log = makeLog(req, logs);
+
+	try {
+		logs[log.ip] = logs[log.ip] || {};
+		logs[log.ip][log.path] = logs[log.ip][log.path] || [];
+		logs[log.ip][log.path].push({
+			time: log.time,
+			loc: `${log.method} ${req.protocol}://${req.hostname}${req.path}`
+		});
+		fs.writeFile('./logs.json', JSON.stringify(logs, null, 2), 'utf8', (err) => {
+			if (err) {
+				throw err;
+			} else {
+				console.log(JSON.stringify(logs, null, 2));
+			}
+		});
+	} catch (error) {
+		console.error(error);
+		next();
+	}
+	next();
+};
+
+module.exports = { getLog, makeLog, logger };
