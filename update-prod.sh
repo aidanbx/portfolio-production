cc7420570484254dd60259a845e7ebe74922de87
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-09-21 12:57:31 -0700

    update prod starts pm2

--- update-prod.sh
+++ update-prod.sh
@@ -1,1 +1,1 @@
-ssh -t aidan@barbieux.dev "cd ~/web/prod-master/ ; rm -rf ./build ; git fetch --all ; git reset --hard origin/master ; exit ; bash"
+ssh -t aidan@barbieux.dev "cd ~/web/prod-master/ ; rm -rf ./build ; git fetch --all ; git reset --hard origin/master ; pm2 restart . ; exit ; bash"
